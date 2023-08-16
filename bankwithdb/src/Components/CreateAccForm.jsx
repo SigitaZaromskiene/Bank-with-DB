2891847d8bc485c087c9c3dbd87626741d3a9d27
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-08-16 11:45:30 +0300

    create btn

--- bankwithdb/src/Components/CreateAccForm.jsx
+++ bankwithdb/src/Components/CreateAccForm.jsx
@@ -1,26 +1,32 @@
 import img from "../img/no-photo.jpg";
+import SmallBtn from "./SmallBtn";
 
 function CreateAccountForm() {
   return (
-    <form className="create-account-form">
-      <div className="account-form-left">
-        <div className="name-surname">
-          <label>Name</label>
-          <input></input>
+    <div className="border">
+      <form className="create-account-form">
+        <div className="account-form-left">
+          <div className="name-surname">
+            <label>Name</label>
+            <input></input>
+          </div>
+          <div className="name-surname">
+            <label>Surname</label>
+            <input></input>
+          </div>
         </div>
-        <div className="name-surname">
-          <label>Surname</label>
-          <input></input>
+        <div className="photo-container">
+          <label>Photo</label>
+          <input className="photo-input" type="file"></input>
+          <div>
+            <img src={img} alt="no-pict" />
+          </div>
         </div>
+      </form>
+      <div>
+        <SmallBtn text="Create"></SmallBtn>
       </div>
-      <div className="photo-container">
-        <label>Photo</label>
-        <input className="photo-input" type="file"></input>
-        <div>
-          <img src={img} alt="no-pict" />
-        </div>
-      </div>
-    </form>
+    </div>
   );
 }
 
